b814950adb70a6b81e643d16cb529f0acd9f2d9e
EkGogoleva <140961533+EkGogoleva@users.noreply.github.com>
2023-09-08 15:10:43 +0400

    Add files via upload

---
+++ Program 1.cs
@@ -0,0 +1,62 @@
+﻿Console.Write("Введите числа через запятую: ");
+int[] Numbers = StringToNum(Console.ReadLine());
+PrintArray (числа);
+целая сумма = 0;
+for (int i = 0; я <numbers.Length; i++)
+{
+    если (числа[i] > 0)
+    {
+        сумма++;
+    }
+}
+Консоль.WriteLine();
+Console.WriteLine($"количество оценок больше 0 = {sum}");
+
+
+int[] StringToNum(строковый ввод)
+{
+    число int = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        если (вход[i] == ',')
+        {
+            считать++;
+        }
+    }
+
+    int[] числа = новый int [count];
+    внутренний индекс = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        строка темп = "";
+
+        в то время как (ввод [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            я++;
+        }
+        еще
+        {
+            temp += input [i].ToString();
+            перерыв;
+        }
+        }
+        числа[индекс] = Convert.ToInt32(temp);
+        индекс++;
+    }
+    обратные номера;
+}
+
+
+void PrintArray (массив int [])
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(массив[i] + " ");
+    }
+    Console.Write("]");
+}
---
+++ Program 2.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых
+// заданных уравнений y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
+двойная точкаX;
+двойная точкаY;
+
+Console.Write("Даны прямые заданные формулы y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Введите по очереди строки b1, k1, b2 и k2 через один пробел: ");
+int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int a = arr [1];
+int b = arr [3];
+int c = arr [0];
+int d = arr [2];
+
+если (а == б)
+{
+    Console.WriteLine("Прямые с такими параметрами паралельны");
+}
+еще
+{
+    точкаX = (двойной) (постоянный ток)/(ab);
+    pointY = (double) a*(dc)/(ab) + c;
+    Console.WriteLine("Координаты точек пересечения двух прямых трансляций:");
+    Console.WriteLine($"Х: {pointX:0.00}");
+    Console.WriteLine($"Y: {pointY:0.00}");
+}
